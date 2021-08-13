8422f279803174efb5e13a09d02941e33064f9f6
Shane Kaestner <shane.kaestner@yahoo.com>
2021-08-13 07:22:29 -0700

    added home and login components to the frontend and set up some routing

---
+++ frontend/todos/todoapp/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = props => {
+
+    return (
+
+        <div className="login-input-form">
+            
+        </div> 
+    )
+}
+
+export default Login;
